ea1c1cf9bf466781d647aa35755fb59f6ccc5397
juanjose999 <juanjose000012@gmail.com>
2024-05-25 13:10:15 -0500

    init project

---
+++ src/main/java/clothes/management/system/in/a/laundry/ClothesManagementSystemInALaundryApplication.java
@@ -0,0 +1,13 @@
+package clothes.management.system.in.a.laundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesManagementSystemInALaundryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesManagementSystemInALaundryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clothes/management/system/in/a/laundry/ClothesManagementSystemInALaundryApplicationTests.java
@@ -0,0 +1,13 @@
+package clothes.management.system.in.a.laundry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesManagementSystemInALaundryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
